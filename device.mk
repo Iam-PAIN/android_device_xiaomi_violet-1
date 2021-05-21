f96e9d80cda84888e00bd29182aac0c31a02e6ae
Md Abu Talha <abu.talha.asif.31@gmail.com>
2021-05-21 07:35:44 +0000

    violet : drop violet parts
    Signed-off-by: Md Abu Talha <abu.talha.asif.31@gmail.com>

--- device.mk
+++ device.mk
@@ -132,9 +132,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
+
 
 # Display
 PRODUCT_PACKAGES += \
@@ -410,10 +408,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
-#VioletParts init
-PRODUCT_PACKAGES += \
-    init.violetparts.sh \
-    init.violetparts.rc
 
 # Wifi
 PRODUCT_PACKAGES += \
